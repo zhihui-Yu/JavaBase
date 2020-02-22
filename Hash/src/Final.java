810e497782659dcbe8a9cddf3180ac648df48bc6
zhihui-Yu <54210717+zhihui-Yu@users.noreply.github.com>
2020-02-22 17:08:20 +0800

    11

---
+++ Hash/src/Final.java
@@ -0,0 +1,13 @@
+
+public class Final extends Person{
+
+	public static void main(String[] args) {
+		Person.ex = new Person();
+	}
+}	
+
+class Person {
+	private String name = "person";
+	public final static Person ex ;
+	
+}
