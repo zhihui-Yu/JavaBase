ab8d7eaa8d3e18248eb22b52001f115606c4f028
zhihui-Yu <54210717+zhihui-Yu@users.noreply.github.com>
2020-03-15 23:02:04 +0800

    update add TreeSet have sort

--- Hash/src/HashSetTest.java
+++ Hash/src/HashSetTest.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * 存储的元素不能重复
@@ -8,18 +9,31 @@ import java.util.HashSet;
  */
 public class HashSetTest {
     public static void main(String[] args) {
+    	//hashSet 乱序
         HashSet<Person> set = new HashSet<Person>();
         Person p1 = new Person("zhangsan", 22);
-        Person p2 = new Person("zhangsan", 22);
-
+        Person p2 = new Person("zhangsan1", 21);
+        Person p3 = new Person("zhangsan2", 20);
+        Person p4 = new Person("zhangsan4", 23);
         set.add(p1);
         set.add(p2);
-
-        System.out.println(set.size());
-
+        set.add(p3);
+        set.add(p4);
+        for (Person person : set) {
+        	System.out.println(person);
+		}
+        //TreeSet 需要对象实现comparable接口 默认升序 可自定义排序
+        TreeSet<Person> ts = new TreeSet<>();
+        ts.add(p1);
+        ts.add(p2);
+        ts.add(p3);
+        ts.add(p4);
+        for (Person person : ts) {
+        	System.out.println(person);
+		}
     }
 }
-class Person {
+class Person implements Comparable<Person>{
     private String name;
     private int age;
 
@@ -57,9 +71,19 @@ class Person {
         }
         return false;
     }
-
+    
     @Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
     public int hashCode() {
         return this.name.hashCode();
     }
+
+	@Override
+	public int compareTo(Person o) {
+		return -((Integer)this.age).compareTo((Integer)o.getAge());
+	}
 }
