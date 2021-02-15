2e7e6c82cf2d845ec5a1c4a5b5d3f315ebe5b5bf
simple <yzhsmail@163.com>
2021-02-15 16:59:12 +0800

    read lock

--- Sync/src/com/lock/ReadLock.java
+++ Sync/src/com/lock/ReadLock.java
@@ -1,6 +1,5 @@
 package com.lock;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -9,16 +8,16 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author simple
  */
 public class ReadLock {
+    static int num = 0;
     public static void main(String[] args) throws InterruptedException {
         ReadWriteLock lock = new ReentrantReadWriteLock(true);
-        AtomicInteger num = new AtomicInteger();
         for (int i = 0; i < 3; i++) {
             new Thread(() -> {
                 lock.readLock();
                 System.out.println("before add -- " + Thread.currentThread().getName() + ": " + num);
                 Lock writeLock = lock.writeLock();
                 writeLock.lock();
-                num.addAndGet(1);
+                num ++;
                 System.out.println("after add -- " + Thread.currentThread().getName() + ": " + num);
                 writeLock.unlock();
             }).start();
